7340be1f7441bfbcb00b39ba5abbaba2a6adbdf2
zzy1024 <45653651+maldovar1024@users.noreply.github.com>
2024-03-09 17:12:31 +0800

    feat: add interpreter cli

---
+++ lox_interpreter_cli/src/main.rs
@@ -0,0 +1,24 @@
+use lox_interpreter::interpret;
+use lox_parser;
+use std::io::{self, Write};
+
+fn main() {
+    loop {
+        print!(">");
+        io::stdout().flush().unwrap();
+        let mut content = String::new();
+        io::stdin().read_line(&mut content).unwrap();
+
+        if content.trim() == "@q" {
+            return;
+        }
+
+        match lox_parser::parse(&content) {
+            Some(ast) => {
+                println!("{ast:?}");
+                println!("{:?}", interpret(&ast));
+            }
+            None => println!("ERROR!"),
+        }
+    }
+}
